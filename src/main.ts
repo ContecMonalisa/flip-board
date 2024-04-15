10a66d830b88c94723166cd6a89e69d533e53784
ihSong <alien0314>
2024-04-15 15:56:09 +0900

    refactor: gasp 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import '@/assets/scss/main.scss';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import gsap from 'gsap';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(gsap);
 app.use(router);
 
 app.mount('#app');
